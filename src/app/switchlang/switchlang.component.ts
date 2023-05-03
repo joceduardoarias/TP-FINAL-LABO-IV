35ea7831958ba9d11d2364b898068ae7c1aa8112
José arias <josee.ariasmelo@gmail.com>
2023-05-03 15:42:50 -0300

    Implementación language translate

--- src/app/switchlang/switchlang.component.ts
+++ src/app/switchlang/switchlang.component.ts
@@ -5,6 +5,7 @@ import {
   Output,
   ViewChild,
 } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 import { log } from 'console';
 
 @Component({
@@ -13,16 +14,18 @@ import { log } from 'console';
   styleUrls: ['./switchlang.component.css'],
 })
 export class SwitchlangComponent implements OnInit {
-  @ViewChild('dropdownContent') dropdownContent: any;
-  language = {Es:"Españo",Pt:"Português",En:"English"};
+  
+  language = {es:"Español",pt:"Português",en:"English"};
   selectedLanguage: string = 'Español';
   flag: string = 'https://flagcdn.com/w320/mx.png';
-  constructor() {}
+  
+  constructor(public translate: TranslateService) {}
 
   ngOnInit(): void {}  
 
   selectLanguage(language: string, flag: string) {        
       if (this.language[language] != undefined) {
+        this.translate.use(language);         
         this.flag = flag;    
         this.selectedLanguage = this.language[language] ;
       }                

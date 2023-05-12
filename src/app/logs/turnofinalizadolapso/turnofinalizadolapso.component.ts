aeb39445555cc4375ec86a55d9f95f3e832cab54
José arias <josee.ariasmelo@gmail.com>
2023-05-12 17:36:30 -0300

    Estilos turnos finalizados

--- src/app/logs/turnofinalizadolapso/turnofinalizadolapso.component.ts
+++ src/app/logs/turnofinalizadolapso/turnofinalizadolapso.component.ts
@@ -1,26 +1,27 @@
-import { Component,ViewChild,ElementRef, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 import jsPDF from 'jspdf';
 import Chart from 'chart.js/auto';
 import { AgregarestadoturnoService } from 'src/app/services/agregarestadoturno.service';
+import html2canvas from 'html2canvas';
 @Component({
   selector: 'app-turnofinalizadolapso',
   templateUrl: './turnofinalizadolapso.component.html',
-  styleUrls: ['./turnofinalizadolapso.component.css']
+  styleUrls: ['./turnofinalizadolapso.component.css'],
 })
 export class TurnofinalizadolapsoComponent implements OnInit {
-
-  listafinal:any = [];
-  myChart:any;
-  fecha:any;
-  correoscontador:any = [];
-  correos:any = [];
-  fechaDesde:any;
-  fechaHasta:any;
-  list:any = [];
-  @ViewChild('content',{static:false}) el!:ElementRef;
-  ctx:any;
-  constructor(private agregarestadoturno:AgregarestadoturnoService) 
-  {
+  listafinal: any = [];
+  myChart: any;
+  fecha: any;
+  correoscontador: any = [];
+  correos: any = [];
+  fechaDesde: any;
+  fechaHasta: any;
+  list: any = [];
+  startDate: Date;
+  endDate: Date;
+  @ViewChild('content', { static: false }) el!: ElementRef;
+  ctx: any;
+  constructor(private agregarestadoturno: AgregarestadoturnoService) {
     // agregarestadoturno.getAll().valueChanges().subscribe(e=>{
     //   for(let i = 0; i<e.length;i++)
     //   {
@@ -30,215 +31,154 @@ export class TurnofinalizadolapsoComponent implements OnInit {
     //     }
     //   }
     //   console.log(e);
-      
     // })
   }
 
-  ngOnInit(): void {
-  }
-  filtrar()
-  {
-
+  ngOnInit(): void {}
+  filtrar() {
     this.listafinal = [];
     this.correos = [];
     this.correoscontador = [];
     this.list = [];
-    let cambai1 = this.fechaDesde.split('/');
-    let eche:any  = cambai1[1]+'/'+cambai1[0]+'/'+cambai1[2];
-    let termineitor1 = new Date(eche);
 
+    let termineitor1 = this.startDate;
+    let termineitor2 = this.endDate;
 
-    
-    let cambai2 = this.fechaHasta.split('/');
-    let eche2:any  = cambai2[1]+'/'+cambai2[0]+'/'+cambai2[2];
-    let termineitor2 = new Date(eche2);
-    
-    
     // var det = new Date("09/05/2024").trans
     // console.log(det);
-    
+
     // var det = new Date(this.fechaDesde)
     // console.log(det);
-    
-  //   var date3 = new Date("09/05/2024")
-  //   var date4 = new Date("09/04/2024")
-  //   // var date1 = new Date("2024/04/08")
-  //   // var date2 = new Date("2024/04/08")
-  //   var f1 =    new Date(2015,11, 31); //31 de diciembre de 2015
-  //   var f2 =    new Date(2014, 10, 30); 
 
-  
-  // console.log(date3);
+    //   var date3 = new Date("09/05/2024")
+    //   var date4 = new Date("09/04/2024")
+    //   // var date1 = new Date("2024/04/08")
+    //   // var date2 = new Date("2024/04/08")
+    //   var f1 =    new Date(2015,11, 31); //31 de diciembre de 2015
+    //   var f2 =    new Date(2014, 10, 30);
 
-  
-  
-    
+    // console.log(date3);
+
     // if(date3 > date4)
     // {
     //   alert("si");
     // }
-    
-    
-    let hola = this.agregarestadoturno.getAll().valueChanges().subscribe(e=>{
-       for(let i = 0; i<e.length;i++)
-       {
-        let data = e[i].dia;
-       let di = data.split('/');
-       let formateado:any  = di[1]+'/'+di[0]+'/'+di[2];
-       let terminado = new Date(formateado);
-  
-    
-    
-       if(terminado > termineitor1 &&  terminado < termineitor2 && e[i].estado == 'finalizado')
-       {
-        this.list.push(e[i]);
-       }
-       }    
 
+    let hola = this.agregarestadoturno
+      .getAll()
+      .valueChanges()
+      .subscribe((e) => {
+        for (let i = 0; i < e.length; i++) {
+          let data = e[i].dia;
+          let di = data.split('/');
+          let formateado: any = di[1] + '/' + di[0] + '/' + di[2];
+          let terminado = new Date(formateado);
 
+          if (
+            terminado > termineitor1 &&
+            terminado < termineitor2 &&
+            e[i].estado == 'finalizado'
+          ) {
+            this.list.push(e[i]);
+          }
+        }
 
-       for(let i = 0; i<this.list.length;i++)
-      {
+        for (let i = 0; i < this.list.length; i++) {
+          const elemento = this.list[i].correoEspecialista;
+          if (!this.correos.includes(this.list[i].correoEspecialista)) {
+            this.correos.push(elemento);
+          }
+        }
 
-        
-       const elemento = this.list[i].correoEspecialista;
-       if(!this.correos.includes(this.list[i].correoEspecialista))
-       {
-        this.correos.push(elemento);
-       }
-       
-      }
+        for (let i = 0; i < this.correos.length; i++) {
+          let contador = 0;
+          for (let j = 0; j < this.list.length; j++) {
+            if (this.correos[i] == this.list[j].correoEspecialista) {
+              contador++;
+            }
+          }
+          this.correoscontador.push(contador);
+        }
+        for (let i = 0; i < this.correos.length; i++) {
+          let data = {
+            correo: this.correos[i],
+            cantidad: this.correoscontador[i],
+          };
+          this.listafinal.push(data);
+        }
 
-      for(let i = 0; i <this.correos.length;i++)
-      {
-         let contador = 0;
-         for(let j = 0; j<this.list.length;j++)
-         {
-           if(this.correos[i] == this.list[j].correoEspecialista)
-           {
-
-            
-             contador ++;
-           }
-         }
-         this.correoscontador.push(contador);
-      }
-      for(let i = 0; i<this.correos.length;i++)
-      {
-        let data = {'correo':this.correos[i],'cantidad':this.correoscontador[i]}
-        this.listafinal.push(data);
-      }
-
-      this.det();
-     hola.unsubscribe()
-    }
-    )
+        this.det();
+        hola.unsubscribe();
+      });
     // setTimeout(() => {
     //   console.log(this.list);
-      
+
     // }, 500);
-
   }
-  det()
-{
-  this.ctx = document.getElementById('myChart');
-  
-  this.ctx.width = "600"
-  this.ctx.height = "300"
-if(this.myChart)
-{
-  this.myChart.destroy();
-}
- this.myChart = new Chart(this.ctx, {
- type: 'pie',
- data: {
-  labels : this.correos,
-  datasets: [{
-    label: 'My First Dataset',
-    data: this.correoscontador,
-    
-    backgroundColor: [
-      'rgb(255, 99, 132)',
-      'rgb(54, 162, 235)',
-      'rgb(100, 203, 86)',
-      'rgb(255, 203, 86)',
-      'rgb(255, 100, 35)',
-      'rgb(0, 0, 35)',
-      'rgb(100, 100, 100,100)',
-      'rgb(200, 200, 200)',
-    ],
-    hoverOffset: 4
-  }]
- },
-});
- // let ctx: any = document.getElementById("myChart") as HTMLElement;
- // var data = {
- //   labels: ["match1", "match2", "match3", "match4", "match5"],
- //   datasets: [
- //     {
- //       label: "TeamA Score",
- //       data: [10, 50, 25, 70, 40],
- //       backgroundColor: "blue",
- //       borderColor: "lightblue",
- //       fill: false,
- //       lineTension: 0,
- //       radius: 5
- //     },
- //     {
- //       label: "TeamB Score",
- //       data: [20, 35, 40, 60, 50],
- //       backgroundColor: "green",
- //       borderColor: "lightgreen",
- //       fill: false,
- //       lineTension: 0,
- //       radius: 5
- //     }
- //   ]
- // };
+  det() {
+    this.ctx = document.getElementById('myChart');
 
- // //options
- // var options = {
- //   responsive: true,
- //   title: {
- //     display: true,
- //     position: "top",
- //     text: "Line Graph",
- //     fontSize: 18,
- //     fontColor: "#111"
- //   },
- //   legend: {
- //     display: true,
- //     position: "bottom",
- //     labels: {
- //       fontColor: "#333",
- //       fontSize: 16
- //     }
- //   }
- // };
-
- // //create Chart class object
- // var chart = new Chart(ctx, {
- //   type: "line",
- //   data: data,
- //   options: options
- // });
-}
-makePDF()
-{
-  this.fecha = new Date().toLocaleDateString()
-  let pdf = new jsPDF('p','pt','a4');
-  const option = {
-    background: 'white',
-    scale: 3,
-  }
-  pdf.html(this.el.nativeElement,{
-    callback:(pdf)=>{
-       pdf.save("Grafico de barras por especialista.pdf")
+    this.ctx.width = '600';
+    this.ctx.height = '300';
+    if (this.myChart) {
+      this.myChart.destroy();
     }
-  });
+    this.myChart = new Chart(this.ctx, {
+      type: 'pie',
+      data: {
+        labels: this.correos,
+        datasets: [
+          {
+            label: 'My First Dataset',
+            data: this.correoscontador,
+
+            backgroundColor: [
+              'rgb(255, 99, 132)',
+              'rgb(54, 162, 235)',
+              'rgb(100, 203, 86)',
+              'rgb(255, 203, 86)',
+              'rgb(255, 100, 35)',
+              'rgb(0, 0, 35)',
+              'rgb(100, 100, 100,100)',
+              'rgb(200, 200, 200)',
+            ],
+            hoverOffset: 4,
+          },
+        ],
+      },
+    });    
+  }
+  makePDF() {
+    const DATA = document.getElementById('myChart');
+    const doc = new jsPDF('p', 'pt', 'a4');
+    const options = {
+      background: 'white',
+      scale: 3,
+    };
+    html2canvas(DATA, options)
+      .then((canvas) => {
+        const img = canvas.toDataURL('image/PNG', 0.8);
+
+        // Add image Canvas to PDF
+        const bufferX = 15;
+        const bufferY = 15;
+        const imgProps = (doc as any).getImageProperties(img);
+        const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+        const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+        doc.addImage(
+          img,
+          'PNG',
+          bufferX,
+          bufferY,
+          pdfWidth,
+          pdfHeight,
+          undefined,
+          'FAST'
+        );
+        return doc;
+      })
+      .then((docResult) => {
+        docResult.save(`${new Date().toISOString()}_turnosSolicitados.pdf`);
+      });
+  }
 }
-
-}
-
-
-

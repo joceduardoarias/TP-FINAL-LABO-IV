20a280a27b4ae26de3f830644a3568bd5193ade7
José arias <josee.ariasmelo@gmail.com>
2023-07-04 14:26:17 -0300

    Refactor

--- src/app/pacientes/historiapaciente/historiapaciente.component.ts
+++ src/app/pacientes/historiapaciente/historiapaciente.component.ts
@@ -4,144 +4,165 @@ import { ExporterService } from 'src/app/services/exporter.service';
 import { HistoriaClinicaService } from 'src/app/services/historia-clinica.service';
 import { RegistrarUsuariosService } from 'src/app/services/registrar-usuarios.service';
 import jsPDF from 'jspdf';
-import {MatTableDataSource} from '@angular/material/table';
-import {MatPaginator} from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-historiapaciente',
   templateUrl: './historiapaciente.component.html',
-  styleUrls: ['./historiapaciente.component.css']
+  styleUrls: ['./historiapaciente.component.css'],
 })
 export class HistoriapacienteComponent implements OnInit {
-  b:any;
-  nuevo:any = [];
-  list:any = [];
-  lista2:any = [];
-  lista3:any = [];
-  activo:boolean = false;
-  fecha = new Date().toLocaleDateString()
-  historiaclinicaa:any = [];
-  @ViewChild('content',{static:false}) el!:ElementRef;
-  tildado:any = "";
-  displayedColumns: string[] = ['altura', 'peso', 'presion', 'temperatura', 'dia','hora','val1','val2', 'val3'];  
-  dataSource:any = new MatTableDataSource<any>();
-  especialista:any;
-  @ViewChild(MatPaginator) paginator: MatPaginator;  
+  b: any;
+  nuevo: any = [];
+  list: any = [];
+  lista2: any = [];
+  lista3: any = [];
+  activo: boolean = false;
+  fecha = new Date().toLocaleDateString();
+  historiaclinicaa: any = [];
+  @ViewChild('content', { static: false }) el!: ElementRef;
+  tildado: any = '';
+  displayedColumns: string[] = [
+    'altura',
+    'peso',
+    'presion',
+    'temperatura',
+    'dia',
+    'hora',
+    'val1',
+    'val2',
+    'val3',
+  ];
+  dataSource: any = new MatTableDataSource<any>();
+  especialista: any;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  constructor(public auth:AuthService,private us:RegistrarUsuariosService,private excelService:ExporterService,private h:HistoriaClinicaService) 
-  { 
+  constructor(
+    public auth: AuthService,
+    private us: RegistrarUsuariosService,
+    private excelService: ExporterService,
+    private h: HistoriaClinicaService
+  ) {
     var users;
-    us.getAll().valueChanges().subscribe((e:any)=>{
-      users = e;      
-      
-      for(let i = 0; i<e.length;i++)
-      {
-        let data = {"nombre":e[i].nombre,"email":e[i].email,"dni":e[i].dni,"perfil":e[i].perfil,"edad":e[i].edad}
-        this.list.push(data);
-      }
-      this.especialista = this.list.find(usuario => usuario.email === auth.correologeado);
-      
-    })
-    h.getAll().valueChanges().subscribe((e:any)=>{
-      for (let i = 0; i < e.length; i++) {
-        const elemento = e[i];
-        
-        if (!this.lista2.some(item => item.correopaciente === elemento.correopaciente)) {
-          this.lista2.push(elemento);
+    us.getAll()
+      .valueChanges()
+      .subscribe((e: any) => {
+        users = e;
+
+        for (let i = 0; i < e.length; i++) {
+          let data = {
+            nombre: e[i].nombre,
+            email: e[i].email,
+            dni: e[i].dni,
+            perfil: e[i].perfil,
+            edad: e[i].edad,
+          };
+          this.list.push(data);
         }
-      }
-            
-      for (var item1 of this.lista2) {
-        for (var item2 of users) {
-          if (item1.correopaciente == item2.email) {
-            item1.imagen = item2.imagen;
+        this.especialista = this.list.find(
+          (usuario) => usuario.email === auth.correologeado
+        );
+      });
+    h.getAll()
+      .valueChanges()
+      .subscribe((e: any) => {
+        for (let i = 0; i < e.length; i++) {
+          const elemento = e[i];
+
+          if (
+            !this.lista2.some(
+              (item) => item.correopaciente === elemento.correopaciente
+            )
+          ) {
+            this.lista2.push(elemento);
           }
         }
-      }
-      (this.lista2);
-      
-     })
-  }
 
-  ngOnInit(): void {
-  }
-  
-  disparo(data:any)
-  {   
-    this.tildado = data;
-    let pec = this.h.getAll().valueChanges().subscribe(e=>{
-      this.historiaclinicaa = [];
-      for(let i = 0; i<e.length;i++)
-      {
-        if(e[i].correopaciente == data)
-        {
-          this.historiaclinicaa.push(e[i]);
-        }
-      }
-      this.dataSource = new MatTableDataSource(this.historiaclinicaa);
-      this.activo = true;
-      (this.historiaclinicaa);
-      
-     })
-  }
-  hacerBusqueda()
-  {
-    this.nuevo = [];
-    for(let j=0;j<this.historiaclinicaa.length;j++)
-      {
-        let no = 0;
-        let encontra2 = 0;
-        for(let i = 0; i<this.historiaclinicaa[j].otros.length;i++)
-        {
-          Object.keys(this.historiaclinicaa[j].otros[i]).forEach((entry,index)=>{
-           if(this.historiaclinicaa[j].otros[i][entry] == this.b)
-           {
-            (encontra2);
-            
-            if(encontra2 == 0)
-            {
-              encontra2++;
-              no = 1;
-              this.nuevo.push(this.historiaclinicaa[j]);
-              (encontra2);
-              
+        for (var item1 of this.lista2) {
+          for (var item2 of users) {
+            if (item1.correopaciente == item2.email) {
+              item1.imagen = item2.imagen;
             }
-
-           }
-          })
-        }
-        if(no == 0)
-        {
-          if(this.historiaclinicaa[j].altura == this.b || this.historiaclinicaa[j].peso == this.b || this.historiaclinicaa[j].presion == this.b  || this.historiaclinicaa[j].temepratura == this.b)
-          {
-            this.nuevo.push(this.historiaclinicaa[j]);
           }
-          
         }
-  
-      }
-      if(this.nuevo.length != 0)
-      {
-        this.historiaclinicaa = this.nuevo
-      }     
+        this.lista2;
+      });
   }
-  limpiar()
-  {
+
+  ngOnInit(): void {}
+
+  disparo(data: any) {
+    this.tildado = data;
+    let pec = this.h
+      .getAll()
+      .valueChanges()
+      .subscribe((e) => {
+        this.historiaclinicaa = [];
+        for (let i = 0; i < e.length; i++) {
+          if (e[i].correopaciente == data) {
+            this.historiaclinicaa.push(e[i]);
+          }
+        }
+        this.dataSource = new MatTableDataSource(this.historiaclinicaa);
+        this.activo = true;
+        this.historiaclinicaa;
+      });
+  }
+  hacerBusqueda() {
+    this.nuevo = [];
+    for (let j = 0; j < this.historiaclinicaa.length; j++) {
+      let no = 0;
+      let encontra2 = 0;
+      for (let i = 0; i < this.historiaclinicaa[j].otros.length; i++) {
+        Object.keys(this.historiaclinicaa[j].otros[i]).forEach(
+          (entry, index) => {
+            if (this.historiaclinicaa[j].otros[i][entry] == this.b) {
+              encontra2;
+
+              if (encontra2 == 0) {
+                encontra2++;
+                no = 1;
+                this.nuevo.push(this.historiaclinicaa[j]);
+                encontra2;
+              }
+            }
+          }
+        );
+      }
+      if (no == 0) {
+        if (
+          this.historiaclinicaa[j].altura == this.b ||
+          this.historiaclinicaa[j].peso == this.b ||
+          this.historiaclinicaa[j].presion == this.b ||
+          this.historiaclinicaa[j].temepratura == this.b
+        ) {
+          this.nuevo.push(this.historiaclinicaa[j]);
+        }
+      }
+    }
+    if (this.nuevo.length != 0) {
+      this.historiaclinicaa = this.nuevo;
+    }
+  }
+  limpiar() {
     this.historiaclinicaa = [];
-    this.h.getAll().get().subscribe(e=>{e.forEach(e=>{
-      if(e.data().correopaciente == this.tildado)
-      {
-        this.historiaclinicaa.push(e.data());
-      }
-    })})
+    this.h
+      .getAll()
+      .get()
+      .subscribe((e) => {
+        e.forEach((e) => {
+          if (e.data().correopaciente == this.tildado) {
+            this.historiaclinicaa.push(e.data());
+          }
+        });
+      });
   }
-  click():void
-  {
-    this.excelService.exportToExcel(this.list,'usuarios');
+  click(): void {
+    this.excelService.exportToExcel(this.list, 'usuarios');
   }
-  makePDF()
-  {     
+  makePDF() {
     const DATA = document.getElementById('tablaPDF');
     const doc = new jsPDF('p', 'pt', 'a4');
     const options = {
@@ -171,7 +192,7 @@ export class HistoriapacienteComponent implements OnInit {
         return doc;
       })
       .then((docResult) => {
-        docResult.save(`${new Date().toISOString()}_HistoriaClínica.pdf`);        
+        docResult.save(`${new Date().toISOString()}_HistoriaClínica.pdf`);
       });
   }
   applyFilter(event: Event) {
@@ -183,4 +204,3 @@ export class HistoriapacienteComponent implements OnInit {
     // }
   }
 }
-
